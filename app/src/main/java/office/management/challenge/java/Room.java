00811f96fffef400022201a623ee3320d73e66b8
Dean Smythe <dean@teamtwisted.co.uk>
2021-11-19 11:43:22 +0000

    Story 4: Rooms are bookable and checkable

--- app/src/main/java/office/management/challenge/java/Room.java
+++ app/src/main/java/office/management/challenge/java/Room.java
@@ -1,14 +1,26 @@
 package office.management.challenge.java;
 
+import java.util.function.BooleanSupplier;
+
 public class Room {
 String name;
+Boolean available;
 
     public Room(String nameIn) {
         name = nameIn;
+        available = true;
     }
 
     public String getName() {
         return this.name;
+    }
+    public void book() {
+        available = false;
+
+    }
+
+    public Boolean isAvailable() {
+        return available;
 
     }
 } // Class
--- app/src/test/java/office/management/challenge/java/RoomTest.java
+++ app/src/test/java/office/management/challenge/java/RoomTest.java
@@ -12,5 +12,15 @@ public class RoomTest {
         Room room = new Room(name);
         assertEquals("Room returns name", room.getName(), name );
     }
-
+    @Test public void roomIsAvailble() {
+        String name = new String("Room1");
+        Room room = new Room(name);
+        assertEquals("New room default is available", room.isAvailable(), true );
+    }
+    @Test public void roomIsNotAvailble() {
+        String name = new String("Room1");
+        Room room = new Room(name);
+        room.book();
+        assertEquals("Booked room is not available", room.isAvailable(), false );
+    }
 }
